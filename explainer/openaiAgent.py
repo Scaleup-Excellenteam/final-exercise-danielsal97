14ebc9914cc68b56584e19c5db07fde166b21cef
danielsal97 <danielsa1997@gmail.com>
2024-07-07 11:36:24 +0300

    automatic expaliner from dir

--- explainer/openaiAgent.py
+++ explainer/openaiAgent.py
@@ -1,7 +1,8 @@
-# openaiAgent.py
-
+import os
 import openai
 import asyncio
+
+from flask.cli import load_dotenv
 
 
 async def pptx_agent(input_text):
@@ -11,15 +12,24 @@ async def pptx_agent(input_text):
     :param input_text: The text input to be processed by the AI model.
     :return: A list of generated text outputs.
     """
-    response = openai.ChatCompletion.create(
+    response = await asyncio.to_thread(
+        openai.ChatCompletion.create,
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "Create paragraphs that explain this:"},
+            {"role": "system", "content": "Create paragraphs that explain this in hebrew:"},
             {"role": "user", "content": input_text}
         ]
     )
-    await asyncio.sleep(3)
     output = [choice['message']['content'].strip() for choice in response.get('choices', [])
               if 'message' in choice and isinstance(choice['message'], dict) and choice['message'].get('content')]
 
     return output
+
+
+def get_key():
+    load_dotenv()
+    env_key = os.getenv('OPENAI_API_KEY')
+    if not env_key:
+        print("API key not found. Please set it in the .env file.")
+        exit(1)
+    return env_key
